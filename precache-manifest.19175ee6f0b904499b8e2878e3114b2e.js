927a4ffbda715ac5f9f47b6901b9da5320d0b03f
srishtirathi <srishti.cs17@bmsce.ac.in>
2020-10-13 14:44:26 +0530

    Updates

---
+++ precache-manifest.19175ee6f0b904499b8e2878e3114b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea7b416d40657718823bf5853c49766",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "28f547851c5766537b26",
+    "url": "/React/static/css/main.bb7f70c8.chunk.css"
+  },
+  {
+    "revision": "9f7c9ef7d6f53abc13f3",
+    "url": "/React/static/js/2.26d94c0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.26d94c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f547851c5766537b26",
+    "url": "/React/static/js/main.d5e806e0.chunk.js"
+  },
+  {
+    "revision": "112cdd67c381f47ffec9",
+    "url": "/React/static/js/runtime-main.ae3eb05b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
